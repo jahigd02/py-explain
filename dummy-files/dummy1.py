7e6884f151576f89ae12ca778580eb230c0b83bd
jahigd02 <jahigd02@louisville.edu>
2022-10-21 02:30:58 -0400

    Added information for import aliases.

--- dummy-files/dummy1.py
+++ dummy-files/dummy1.py
@@ -1,3 +1,5 @@
 import os
 
 from os import system
+
+import pandas as pd
--- py-explain.py
+++ py-explain.py
@@ -1,6 +1,7 @@
-# fileLines is a list of tuples: the first element in a tuple is the line string, and the second element is a list of
+# fileLines is a list of tuples: the first element in the tuple is the line string, and the second element is a list of
 # all tokens from the line.
 fileLines = []
+
 
 # Pretty print
 def pp(line_number, line_str, message):
@@ -17,9 +18,15 @@ def read_file(filepath):
 
 def import_statement(line_split):
     if line_split[0] == "import":
-        return f"Imports the {line_split[1]} module."
+        message = f"Imports the \"{line_split[1]}\" module"
+        if "as" in line_split:
+            alias = line_split[line_split.index("as") + 1]
+            message = message + f", and gives it the alias \"{alias}\""
+        message = message + "."
+        return message
     elif line_split[0] == "from":
-        return f"Imports the {line_split[3]} section from the {line_split[1]} module."
+        return f"Imports the \"{line_split[3]}\" section from the \"{line_split[1]}\" module."
+
 
 method_dict = {
     'import_statement': import_statement,
@@ -30,16 +37,20 @@ def analyze(line_split):
         return method_dict["import_statement"](line_split)
 
 
-def parseInstructions(fileLines):
+def parse_instructions(fileLines, include_whitespace=False):
     for line_number, line_tup in enumerate(fileLines):
         line_str = line_tup[0]
         line_split = line_tup[1]
 
         if len(line_str.strip()) == 0:
-            pp(line_number, "", "Empty line.")
+            if include_whitespace is not False:
+                pp(line_number, "", "Empty line.")
+            else:
+                pass
         else:
             message = analyze(line_split)
             pp(line_number, line_str, message)
 
+
 read_file("dummy-files/dummy1.py")
-parseInstructions(fileLines)
+parse_instructions(fileLines, include_whitespace=True)
